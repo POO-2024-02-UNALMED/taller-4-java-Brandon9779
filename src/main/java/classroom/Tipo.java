7bba2e2156a07244b161046a1815a6e8ab928ab5
Brandon9779 <elbrandon9779@gmail.com>
2024-11-24 23:12:38 -0500

    Update Tipo.java

--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -6,17 +6,13 @@ public enum Tipo {
     private int codigo;
     private String nombre;
 
-  
-     Tipo(int codigo) {
+
+    Tipo(int codigo) {
         this.codigo = codigo;
-        this.nombre = this.name(); 
     }
 
-    public int getCodigo() {
-        return codigo;
-    }
 
-    public String getNombre() {
-        return nombre;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 }
