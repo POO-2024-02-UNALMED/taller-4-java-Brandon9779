61169099c2a5b6b82579baffe41b6bb4ac74fee3
Brandon9779 <elbrandon9779@gmail.com>
2024-11-24 23:33:16 -0500

    Update Tipo.java

--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -1,12 +1,25 @@
 package classroom;
 
 public enum Tipo {
-	DISCIPLINAR(10),FUNDAMENTACION(20),ELECTIVA(30);
-	
-	private int codigo;
-	private String nombre;
-	public Tipo(int codigo,String nombre) {
-		this.codigo = codigo;
-		this.nombre = nombre;
-	}
+    DISCIPLINAR(10, "Disciplinar"),
+    FUNDAMENTACION(20, "Fundamentación"),
+    ELECTIVA(30, "Electiva");
+
+    private int codigo;
+    private String nombre;
+
+    // Constructor modificado para incluir un valor por defecto en el nombre
+    Tipo(int codigo, String nombre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+    }
+
+    // Métodos getter
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
 }
