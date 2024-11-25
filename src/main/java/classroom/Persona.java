8d0756d4196b7acf5101521038ca825011ff99cc
Brandon9779 <elbrandon9779@gmail.com>
2024-11-24 23:04:38 -0500

    Update Persona.java

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -4,26 +4,26 @@ public class Persona {
 
     final long cedula;
     String nombre;
-    static int totalPersonas;
+    static int totalPersonas = 0;
 
-    static {
-        totalPersonas = 0;
-    }
 
-    public Persona() {
+    public Persona(int i, String string) {
         this.cedula = totalPersonas++;  
         this.nombre = "";
     }
+
 
     public Persona(String nombre) {
         this.cedula = totalPersonas++;  
         this.nombre = nombre;
     }
 
+ 
     public Persona(long cedula) {
         this.cedula = cedula;
         this.nombre = "";
     }
+
 
     public Persona(String nombre, long cedula) {
         this.cedula = cedula;
