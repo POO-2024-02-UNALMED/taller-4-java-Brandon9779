73da0638cf2a0e233c41166a5267f1fd8487881b
Brandon9779 <elbrandon9779@gmail.com>
2024-11-24 23:26:40 -0500

    Update Tipo.java

--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -1,18 +1,12 @@
 package classroom;
 
 public enum Tipo {
-    DISCIPLINAR(10), FUNDAMENTACION(20), ELECTIVA(30);
-
-    private int codigo;
-    private String nombre;
-
-
-    Tipo(int codigo) {
-        this.codigo = codigo;
-    }
-
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+	DISCIPLINAR(10),FUNDAMENTACION(20),ELECTIVA(30);
+	
+	private int codigo;
+	private String nombre;
+	public Tipo(int codigo,String nombre) {
+		this.codigo = codigo;
+		this.nombre = nombre;
+	}
 }
