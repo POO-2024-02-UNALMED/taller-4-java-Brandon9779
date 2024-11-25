a3dbd5d67b03b36d43de20276b46005ba6102001
Brandon9779 <elbrandon9779@gmail.com>
2024-11-25 02:35:07 -0500

    Update Asignatura.java

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -1,30 +1,33 @@
 package classroom;
 
 public class Asignatura {
-    public String nombre = "Sin nombre";
-    public int codigoInterno = 0;
-    public int codigoExterno = 0;
+    public String nombre;
+    public int codigoInterno;
+    public int codigoExterno;
     public Tipo tipo;
 
+    // Constructor 1: Por defecto
     public Asignatura() {
         this.nombre = "Sin nombre";
-        this.tipo = Tipo.FUNDAMENTACION;
         this.codigoExterno = 0;
+        this.tipo = Tipo.FUNDAMENTACION;
     }
 
+    // Constructor 2: Nombre y tipo
     public Asignatura(String nombre, Tipo tipo) {
         this.nombre = nombre;
         this.tipo = tipo;
-        this.codigoExterno = 0;
+        this.codigoExterno = 0; // Según el test, debe ser 0
     }
 
+    // Constructor 3: Código externo y tipo
     public Asignatura(int codigoExterno, Tipo tipo) {
-        this.nombre = "Sin nombre";
-        this.tipo = tipo;
+        this.nombre = "Sin nombre"; // Según el test, debe ser "Sin nombre"
         this.codigoExterno = codigoExterno;
+        this.tipo = tipo;
     }
 
-
+    // Constructor 4: Nombre, código externo, código interno y tipo
     public Asignatura(String nombre, int codigoExterno, int codigoInterno, Tipo tipo) {
         this.nombre = nombre;
         this.codigoExterno = codigoExterno;
@@ -32,18 +35,7 @@ public class Asignatura {
         this.tipo = tipo;
     }
 
-    public void setCodigoInterno(int codigoInterno) {
-        this.codigoInterno = codigoInterno;
-    }
-
-    public void cambiarDatos(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void cambiarDatos(int codigoExterno) {
-        this.codigoExterno = codigoExterno;
-    }
-
+    // Getters y Setters
     public String getNombre() {
         return nombre;
     }
@@ -56,12 +48,20 @@ public class Asignatura {
         return codigoInterno;
     }
 
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
     public void setCodigoExterno(int codigoExterno) {
         this.codigoExterno = codigoExterno;
     }
 
-    public Tipo getTipo() {
-        return tipo;
+    public void setCodigoInterno(int codigoInterno) {
+        this.codigoInterno = codigoInterno;
     }
 
     public void setTipo(Tipo tipo) {
